faf0fc5d95099d302d64e70f95d978a1fbb2f9db
sarathskumar125 <sarathskumar125@gmail.com>
2022-11-12 07:40:54 +0530

    Amazon-clone-React-project

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+       <header>
+        <a href="/">Amazon</a>
+      </header>
+      <main>List products</main>
+    </div>
+  )
+}
+
+export default App
